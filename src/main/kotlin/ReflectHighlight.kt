11d8b688b6c389bfa96b72614cbd90a5cf9f6a7c
cak <cak@fastmail.com>
2020-05-16 05:07:25 -0400

    Highlight header or body

--- src/main/kotlin/ReflectHighlight.kt
+++ src/main/kotlin/ReflectHighlight.kt
@@ -4,15 +4,21 @@ import org.parosproxy.paros.network.HttpMessage
 import org.parosproxy.paros.view.View
 import org.zaproxy.zap.extension.search.SearchMatch
 
-class ReflectHighlight(private val reqRes: HttpMessage, private val tokens: List<String>) {
+class ReflectHighlight(
+    private val reqRes: HttpMessage,
+    private val headerTokens: List<String>,
+    private val bodyTokens: List<String>
+) {
 
-    private val searchMatch: SearchMatch? by lazy {
-        searchMatcher(tokens)
+    private val searchMatch: Pair<SearchMatch?, SearchMatch?> by lazy {
+        Pair(searchMatcher(headerTokens), searchMatcher(bodyTokens))
     }
 
     fun highlight() {
-        searchMatch?.let {
-            View.getSingleton().responsePanel.highlightBody(it)
+        if (searchMatch.second != null) {
+            View.getSingleton().responsePanel.highlightBody(searchMatch.second)
+        } else if (searchMatch.first != null) {
+            View.getSingleton().responsePanel.highlightHeader(searchMatch.first)
         }
     }
 
