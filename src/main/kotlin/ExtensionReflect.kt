f0fc940931fcc75ae6bb0c4f071b3fa548e55aa7
cak <cak@fastmail.com>
2020-06-10 20:36:12 -0400

    Fix unload listener

--- src/main/kotlin/ExtensionReflect.kt
+++ src/main/kotlin/ExtensionReflect.kt
@@ -3,10 +3,12 @@ package org.zaproxy.zap.extension.reflect
 import org.parosproxy.paros.extension.ExtensionAdaptor
 import org.parosproxy.paros.extension.ExtensionHook
 import org.parosproxy.paros.network.HttpSender
+import java.awt.EventQueue
 
 class ExtensionReflect : ExtensionAdaptor(NAME) {
 
     private val reflectPanel = ReflectPanel()
+    private val reflectListener = ReflectListener(reflectPanel)
 
     companion object {
         const val NAME = "Reflect"
@@ -15,13 +17,13 @@ class ExtensionReflect : ExtensionAdaptor(NAME) {
     override fun hook(extensionHook: ExtensionHook?) {
         super.hook(extensionHook)
         if (view != null) {
-            HttpSender.addListener(ReflectListener(reflectPanel))
+            HttpSender.addListener(reflectListener)
             extensionHook?.hookView?.addStatusPanel(reflectPanel)
         }
     }
 
     override fun unload() {
-        HttpSender.removeListener(ReflectListener(reflectPanel))
+        HttpSender.removeListener(reflectListener)
     }
 
     override fun canUnload(): Boolean = true
