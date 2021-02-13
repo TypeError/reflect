d328f35435ae678c48e5cd4af8788bad36494e9b
cak <cak@fastmail.com>
2021-02-13 06:09:26 -0500

    Refactor and handle IllegalArgumentException

--- src/main/kotlin/ReflectListener.kt
+++ src/main/kotlin/ReflectListener.kt
@@ -1,6 +1,7 @@
 package org.zaproxy.zap.extension.reflect
 
 import org.apache.commons.httpclient.URI
+import org.apache.log4j.Logger
 import org.parosproxy.paros.network.HtmlParameter
 import org.parosproxy.paros.network.HttpHeader
 import org.parosproxy.paros.network.HttpMessage
@@ -28,68 +29,81 @@ data class ReflectedResponse(
 
 class ReflectListener(private val reflectPanel: ReflectPanel) : HttpSenderListener {
 
+    private val logger: Logger = Logger.getLogger(ReflectListener::class.java)
+
     override fun onHttpResponseReceive(msg: HttpMessage?, initiator: Int, sender: HttpSender?) {
         msg?.let { req ->
-            if (req.isInScope || reflectPanel.scopeCheckbox.isSelected) {
-                val params = mutableSetOf<HtmlParameter>()
-                params.addAll(req.urlParams)
-                params.addAll(req.formParams)
-                val filteredParams = params.asSequence().filter { it.value.length >= 4 }
-                val responseHeader = req.responseHeader.toString()
-                val responseBody = req.responseBody.toString()
-                val reflectedHeader = filteredParams
-                    .filter {
-                        responseHeader.contains(
-                            it.value,
-                            ignoreCase = true
-                        ) || responseHeader.contains(
-                            URLDecoder.decode(
-                                it.value.replace("%", "%25"), "UTF-8"
-                            ),
-                            ignoreCase = true
-                        )
-                    }.toSet()
-                val reflectedBody = filteredParams
-                    .filter {
-                        responseBody.contains(
-                            it.value,
-                            ignoreCase = true
-                        ) || responseBody.contains(
-                            URLDecoder.decode(it.value, "UTF-8"),
-                            ignoreCase = true
-                        )
-                    }.toSet()
-                val reflectedHeaderTokens = reflectedHeader.map { it.value }.toSet().toList()
-                val reflectedBodyTokens = reflectedBody.map { it.value }.toSet().toList()
-                val reflected = (reflectedHeader + reflectedBody).toSet().toList()
-                if (reflected.isNotEmpty()) {
-                    val now = LocalDateTime.now()
-                    val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                    val dateTime = now.format(dateFormatter) ?: ""
-                    val parameters = reflected.joinToString { "${it.name}=${it.value}" }
-                    val length = if (req.responseHeader.contentLength != -1) {
-                        req.responseHeader.contentLength.toString()
-                    } else {
-                        ""
-                    }
-                    val reqRes = ReflectedResponse(
-                        msg = req,
-                        dateTime = dateTime,
-                        host = req.requestHeader?.uri?.host ?: "",
-                        url = req.requestHeader?.uri,
-                        path = req.requestHeader.uri.path,
-                        method = req.requestHeader?.method ?: "",
-                        statusCode = req.responseHeader.statusCode,
-                        title = getTitle(responseBody),
-                        length = length,
-                        mimeType = req.responseHeader.getHeaderValues(HttpHeader.CONTENT_TYPE).toString(),
-                        protocol = req.requestHeader.uri.scheme,
-                        parameters = parameters,
-                        highlighter = ReflectHighlight(req, reflectedHeaderTokens, reflectedBodyTokens)
-                    )
-                    reflectPanel.addReflection(reqRes)
+            findReflection(req)
+        }
+    }
+
+    private fun findReflection(req: HttpMessage) {
+        if (req.isInScope || reflectPanel.scopeCheckbox.isSelected) {
+            val params = mutableSetOf<HtmlParameter>()
+            params.addAll(req.urlParams)
+            params.addAll(req.formParams)
+            val filteredParams = params.asSequence().filter { it.value.length >= 4 }
+            val responseHeader = req.responseHeader.toString()
+            val responseBody = req.responseBody.toString()
+
+            val reflectedHeader = filteredParams
+                .filter {
+                    reflectionFound(req, responseHeader, it.value)
+                }.toSet()
+
+            val reflectedBody = filteredParams
+                .filter {
+                    reflectionFound(req, responseBody, it.value)
+                }.toSet()
+
+            val reflectedHeaderTokens = reflectedHeader.map { it.value }.toSet().toList()
+            val reflectedBodyTokens = reflectedBody.map { it.value }.toSet().toList()
+            val reflected = (reflectedHeader + reflectedBody).toSet().toList()
+            if (reflected.isNotEmpty()) {
+                val now = LocalDateTime.now()
+                val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                val dateTime = now.format(dateFormatter) ?: ""
+                val parameters = reflected.joinToString { "${it.name}=${it.value}" }
+                val length = if (req.responseHeader.contentLength != -1) {
+                    req.responseHeader.contentLength.toString()
+                } else {
+                    ""
                 }
+                val reqRes = ReflectedResponse(
+                    msg = req,
+                    dateTime = dateTime,
+                    host = req.requestHeader?.uri?.host ?: "",
+                    url = req.requestHeader?.uri,
+                    path = req.requestHeader.uri.path,
+                    method = req.requestHeader?.method ?: "",
+                    statusCode = req.responseHeader.statusCode,
+                    title = getTitle(responseBody),
+                    length = length,
+                    mimeType = req.responseHeader.getHeaderValues(HttpHeader.CONTENT_TYPE).toString(),
+                    protocol = req.requestHeader.uri.scheme,
+                    parameters = parameters,
+                    highlighter = ReflectHighlight(req, reflectedHeaderTokens, reflectedBodyTokens)
+                )
+                reflectPanel.addReflection(reqRes)
             }
+        }
+    }
+
+    private fun reflectionFound(req: HttpMessage, response: String, paramValue: String): Boolean {
+        if (response.contains(paramValue)) {
+            return true
+        }
+
+        return try {
+            response.contains(
+                URLDecoder.decode(paramValue, "UTF-8"), ignoreCase = true
+            )
+        } catch (e: IllegalArgumentException) {
+            logger.warn(
+                "An error occurred URL decoding the parameter value: $paramValue on ${req.requestHeader?.uri} [Reflect add-on]",
+                e
+            )
+            return false
         }
     }
 
@@ -103,4 +117,3 @@ class ReflectListener(private val reflectPanel: ReflectPanel) : HttpSenderListen
 
     override fun onHttpRequestSend(msg: HttpMessage?, initiator: Int, sender: HttpSender?) {}
 }
-
